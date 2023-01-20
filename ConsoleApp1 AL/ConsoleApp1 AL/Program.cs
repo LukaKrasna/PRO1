2fbb032c61da934f9c31a6490d4f5e02f77052b6
Luka Krašna <luka.krasna02@gmail.com>
2023-01-20 09:49:15 +0100

    Vaja2

---
+++ ConsoleApp1 AL/ConsoleApp1 AL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_AL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Navedi konec zaporednja");
+            int n = int.Parse(Console.ReadLine());
+            while()
+            
+        }
+    }
+}
---
+++ Dmald/Dmald/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dmald
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald farmo ima.");
+            Console.WriteLine("IAIAO.");
+            Console.WriteLine("Na tej farmi osla dva.");
+            Console.WriteLine("IAIAO.");
+            Console.ReadLine();
+        }
+    }
+}
