952a14279b73fce730f1d421e55bb3b98d8d33d4
Luka Krašna <luka.krasna02@gmail.com>
2023-01-27 11:01:55 +0100

    Vaja

---
+++ nalogica/nalogica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nalogica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double a = 20;
+            //Console.WriteLine("a={0,20:f2}", a);
+            //double b = -13.67;
+            //Console.WriteLine("b={0,20:f2}", b);
+            //double c = 17;
+            //Console.WriteLine("c={0,20:f2}", c);
+            //double d = 12.88;
+            //Console.WriteLine("d={0,20:f2}", d);
+            //double e = 222.88;
+            //Console.WriteLine("e={0,20:f2}", e);
+            //Console.ReadLine();
+
+            char znak = '?';
+            int ascii = 69;
+            Console.WriteLine(znak + " ima ascii kodo " + ascii + ".");
+            Console.WriteLine(ascii + " ima ascii kodo " + znak + ".");
+            Console.ReadLine(); 
+        }
+    }
+}
--- vajaizpis/vajaizpis/Program.cs
+++ vajaizpis/vajaizpis/Program.cs
@@ -15,6 +15,15 @@ namespace vajaizpis
             Console.WriteLine("a=" + a + " b=" + b);
             //formatiranje izpisa
             Console.WriteLine("a={0} b={1}", a, b);
+            Console.WriteLine("a={0,6:d} b={1,6:d}", a, b);
+            Console.WriteLine("a={0,6:x} b={1,6:x}", a, b);
+            double r = 12345.6789;
+            Console.WriteLine("r={0,10:f2}", r);
+            Console.WriteLine("r={0,10:e2}", r);
+            Console.WriteLine("r={0,10:g2}", r);
+            Console.WriteLine("r={0,10:n2}", r);
+            double p = 0.25;
+            Console.WriteLine("p={0,10:p2}", p);
             Console.ReadLine();
         }
     }
