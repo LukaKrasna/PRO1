56cc301931d833fd4c6e99c46552878434e7a146
Luka Krašna <luka.krasna02@gmail.com>
2023-02-03 10:23:09 +0100

    Vaja10

---
+++ ConsoleApp AL/ConsoleApp AL/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_AL
+{
+    public class Mojr
+    {
+        private string mojniz;
+        public Mojr(string niz)
+        {
+            mojniz = niz;
+        }
+        public void Izpis()
+        {
+            Console.WriteLine(mojniz);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mojr prvi;
+            prvi = new Mojr("Moj prvi objekt v C#");
+            prvi.Izpis(); 
+        }
+    }
+}
---
+++ Vaja10/Vaja10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vnesi število za a stranico.");
+            string aa = Console.ReadLine();
+            int a = int.Parse(aa);
+            Console.WriteLine("Vnesi število za b stranico.");
+            string bb = Console.ReadLine();
+            int b = int.Parse(bb);
+            Console.WriteLine("Vnesi število za c stranico.");
+            string cc = Console.ReadLine();
+            int c = int.Parse(cc);
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                Console.WriteLine("So stranice trikotnika.");
+            }
+            else
+            {
+                Console.WriteLine("Niso straice trikotnika.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
