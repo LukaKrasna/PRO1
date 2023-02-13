d9c09d37fc220faa0bbe642dd16140c8ae0767fa
Luka Krašna <luka.krasna02@gmail.com>
2023-02-13 10:11:03 +0100

    l

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            double d = 0;
+            Random r = new Random();
+            for (int k=0; k<=99; k++)
+            {
+                int št = r.Next(100);
+                x = x + št;
+            }
+            d = x / 100;
+            Console.WriteLine("Vsota je " + d);
+        }
+    }
+}
