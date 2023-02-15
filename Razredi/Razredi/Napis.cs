161c5fe8edfc13ed5647ca7ae4b59673518c9ab0
Luka Krašna <luka.krasna02@gmail.com>
2023-02-15 12:41:05 +0100

    15. 2. 2023

---
+++ Razredi/Razredi/Napis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razredi
+{
+    internal class Napis
+    {
+        public string najljubšiProgram; //vezano na objekt
+        public static int štIzvodov; // vezano na razred
+        public void izpiši()
+        {
+            Console.WriteLine("Rad imam " + najljubšiProgram);
+        }
+        public Napis() //to je konstruktor. Ustvariš ga s napisom ctor (tab 2x).
+        {
+            najljubšiProgram = "Neznano";
+            Console.WriteLine("Konstruktor brez parametra");
+            štIzvodov++;
+        }
+        public Napis(string n)
+        {
+            najljubšiProgram = n;
+            Console.WriteLine("Konstruktor s parametri");
+            štIzvodov++;
+        }
+        public static void Koliko()
+        {
+            Console.WriteLine("Število objektov tipa Napis je " + štIzvodov);
+        }
+        /*~Napis() //destruktor (ni treba znat)
+        {
+            Console.WriteLine("Sem v destruktorju.");
+        }*/
+    }
+}
---
+++ Razredi/Razredi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razredi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Napis x = new Napis();
+            x.najljubšiProgram = "C#";
+            x.izpiši();
+            Napis.Koliko();
+            Napis y = new Napis();
+            y.najljubšiProgram = "python";
+            y.izpiši();
+            Napis.Koliko();
+            Napis.Koliko();
+            Napis.štIzvodov = 5;
+            /*Console.WriteLine("Kateri je tvoj najboljši p. j.?");
+            string moj = Console.ReadLine();
+            Napis z = new Napis(moj);
+            z.izpiši(); */
+            Console.ReadLine();
+        }
+    }
+}
