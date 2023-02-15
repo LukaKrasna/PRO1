29be3d50250bbc84afe72d5500986ff0bde8ef3d
Luka Krašna <luka.krasna02@gmail.com>
2023-02-15 12:59:14 +0100

    15. 2. 2023

--- Liki/Liki/Program.cs
+++ Liki/Liki/Program.cs
@@ -10,6 +10,7 @@ namespace Liki
     {
         static void Main(string[] args)
         {
+            /* //1. naloga:
             Krog k = new Krog(37, 45, 2.5f);
             Console.WriteLine("X koordinata = " + k.X +
             "\nY koordinata = " + k.Y +
@@ -25,7 +26,15 @@ namespace Liki
             "\nRadij =" + k.Radij);
             Console.WriteLine("Premer = " + k.Premer());
             Console.WriteLine("Plošcina =" + k.Ploščina());
-            Console.ReadLine();
+            Console.ReadLine();*/
+
+            //2. naloga:
+            Pravokotnik p = new Pravokotnik(0, 1, 1, 0);
+            Console.WriteLine("Koordinati kota levo zgoraj = " + p.X1 + "," + p.Y1 +
+            "\nKoordinati kota desno spodaj = " + p.X2 + "," + p.Y2);
+            Console.WriteLine("Stranica a " + p.StranicaA());
+            Console.WriteLine("Stranica b " + p.StranicaB());
+            Console.WriteLine("Ploščina " + p.Ploščina());
         }
     }
 }
