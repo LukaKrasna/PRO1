82478376e08bd02e9b3d03b782b5163af2f991bd
Luka Krašna <luka.krasna02@gmail.com>
2023-02-20 10:29:14 +0100

    20. 2. 2023

---
+++ Dodatne vaje razredi - lahke 2. naloga/Dodatne vaje razredi - lahke 2. naloga/Pravokotnik.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dodatne_vaje_razredi___lahke_2.naloga
+{
+    internal class Pravokotnik
+    {
+        /*Ustvarite razred Pravokotnik, ki predstavlja pravokotnik z dolžino in širino.Razred
+Pravokotnik mora biti enkapsuliran, tako da dolžine in širine ni mogoče spremeniti, ko ju
+nastavite s konstruktorjem.Razred mora zagotoviti tudi naslednje metode:
+GetArea: vrne površino pravokotnika.
+GetPerimeter: vrne obseg pravokotnika.
+IsSquare: vrne true, če je pravokotnik kvadrat (tj.dolžina = širina), in false v nasprotnem
+primeru.
+Dodajte metodo Main in testirajte delovanje.*/
+        int dolžina;
+        int širina;
+
+        public int Dolžina { get => dolžina; }
+        public int Širina { get => širina; }
+        public Pravokotnik(int d, int s)
+        {
+            dolžina = d;
+            širina = s;
+        }
+        public double GetArea()
+        {
+            return dolžina * širina;
+        }
+        public double GetPerimeter()
+        {
+            return dolžina + dolžina + širina + širina;
+        }
+        public bool IsSquare()
+        {
+            if (dolžina == širina)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Dodatne vaje razredi - lahke 2. naloga/Dodatne vaje razredi - lahke 2. naloga/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dodatne_vaje_razredi___lahke_2.naloga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pravokotnik a = new Pravokotnik(14, 12);
+            Console.WriteLine("Kakšna je ploščina? " + a.GetArea() + "cm");
+            Console.WriteLine("Kakšen je obseg? " + a.GetPerimeter() + "cm");
+            Console.WriteLine("Ali je to kvadrat? " + a.IsSquare());
+            Console.ReadLine();
+        }
+    }
+}
