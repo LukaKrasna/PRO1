2650bc043ff7af509cf8543de0b95bbc942af148
Luka Krašna <luka.krasna02@gmail.com>
2023-02-22 11:46:14 +0100

    22. 2. 2023

---
+++ Dedovanje/Dedovanje/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dedovanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delavec d = new Delavec("123", "Janez Novak", 10, 160);
+            d.Izpis();
+            LDelavec a = new LDelavec();
+            a.DelID = "5678";
+            a.Ime = "Miha Potrč";
+            a.LetnaPlača = 12000;
+            a.Izpis();
+            Console.ReadLine();
+        }
+    }
+}
