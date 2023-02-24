8fd7e7de0f6f81662b377fe45ce77a3051802960
Luka Krašna <luka.krasna02@gmail.com>
2023-02-24 11:42:54 +0100

    24. 2. 2023

---
+++ Vaje dedovanje, abstraktni razredi, vmesnik 2/Vaje dedovanje, abstraktni razredi, vmesnik 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaje_dedovanje__abstraktni_razredi__vmesnik_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2. Gradite aplikacijo za upravljanje zalog v trgovini. Na voljo sta dve vrsti izdelkov: knjige in igrače.
+Obe vrsti izdelkov morata imeti edinstven ID, ime in ceno.Razred Book in razred Toy morata
+dedovati od abstraktnega razreda AbstractProduct, ki implementira vmesnik IProduct.
+Vmesnik IProduct mora definirati naslednje metode:
+GetID(): vrne ID izdelka.
+GetName(): vrne ime izdelka.
+GetPrice(): vrne ceno izdelka.
+Abstraktni razred AbstractProduct bi moral implementirati vmesnik IProduct in definirati naslednje
+abstraktne metode:
+GetDescription(): vrne opis izdelka.
+GetDiscountedPrice(): vrne ceno izdelka po uporabi morebitnih popustov.
+Razreda Book in Toy morata izvajati metodi GetDescription() in GetDiscountedPrice(), medtem ko
+mora razred AbstractProduct definirati metodo GetCategory(), ki vrne kategorijo izdelka(bodisi
+"knjiga" ali "igrača").
+V metodi Main testirajte delovanje.*/
+        }
+    }
+}
---
+++ Vaje dedovanje, abstraktni razredi, vmesnik 2/Vaje dedovanje, abstraktni razredi, vmesnik 2/Toy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaje_dedovanje__abstraktni_razredi__vmesnik_2
+{
+    internal class Toy
+    {
+    }
+}
