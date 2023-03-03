c8d53c2504b71a4f9c8f034db6ea96660f76f6a7
Luka Krašna <luka.krasna02@gmail.com>
2023-03-03 11:33:42 +0100

    3. 3. 2023

--- Vaja tabele/Vaja tabele/Program.cs
+++ Vaja tabele/Vaja tabele/Program.cs
@@ -9,7 +9,21 @@ namespace Vaja_tabele
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {/*1. Napišite program, v katerem imate deklarirano spremenljivko a, ki je tabela
+cela števil. Vsebuje naj 10 elementov.
+a. napišite kodo s pomočjo katere bo uporabnik vnesel 10 števil. Števila
+shranite v tabelo a. Program naj uporabnika vpraša : "Vnesi število 1 ",
+nato počaka na vnos, nato "Vnesi število 2 " itd.
+b. napišite del kode, kjer boste poiskali in izpisali najmanjši in največji
+element tabele
+c. napišite kodo, kjer boste izračunali in izpisali vsoto in povprečno
+vrednost elementov tabele. */
+            int[] a = new int[10];
+            Console.WriteLine("Vnesi 10 števil.");
+            for(int b = 0;b <= 10; b++)
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
