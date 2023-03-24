ff3b1eb6d6d723e072590259fd43ec88c266a5d2
Luka Krašna <luka.krasna02@gmail.com>
2023-03-24 11:04:41 +0100

    24. 3. 2023

---
+++ Koncerti/Koncerti/NakupKarte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koncerti
+{
+    enum PrivilegeLevel {Standard, Premium, Executive, PremiumExecutive}
+    internal class NakupKarte
+    {
+        string imeDogodka;
+        string štKupca;
+        int štVstopnic;
+        PrivilegeLevel privilegij;
+        public string ŠtKupca { get => štKupca;
+        set
+            { if (String.IsNullOrEmpty(value))
+                    throw new Exception("Ni kupca");
+                else
+                    štKupca = value;
+            } 
+        }
+
+    }
+}
