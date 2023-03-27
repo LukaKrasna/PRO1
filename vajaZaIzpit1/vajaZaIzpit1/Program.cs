199f21ee28d913c55db6346117387251be77aed0
Luka Krašna <luka.krasna02@gmail.com>
2023-03-27 09:55:45 +0200

    27. 3. 2023

---
+++ vajaZaIzpit1/vajaZaIzpit1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vajaZaIzpit1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metalec m = new Metalec(1, "Kozmus", "Primož", "Ilirija", "met klavida");
+            m.vnesiRezultat();
+            m.Izpis();
+            Console.WriteLine("Najdaljši met "+m.NajboljšiMet());
+            Console.ReadLine();
+
+        }
+    }
+}
